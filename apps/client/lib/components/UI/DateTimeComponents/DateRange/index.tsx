751f56c8fc55824123f4f9d2dc353a7673e7201e
Drex <david@invent3.ai>
2024-12-06 19:09:09 +0100

    Chore: Worked on the date range modal

--- apps/client/lib/components/UI/DateTimeComponents/DateRange/index.tsx
+++ apps/client/lib/components/UI/DateTimeComponents/DateRange/index.tsx
@@ -1,37 +1,122 @@
-'use client';
+"use client"
+import { HStack, ModalBody, ModalFooter, Text, VStack } from '@chakra-ui/react';
 import { addDays } from 'date-fns';
 import { useState } from 'react';
-import { DateRangePicker } from 'react-date-range';
+import { DateRangePicker, Range } from 'react-date-range';
+import Button from '../../Button';
+import GenericModal from '../../Modal';
 import './styles.css';
-import './default.css';
-const DateRange = () => {
-  const [state, setState] = useState([
-    {
-      startDate: new Date(),
-      endDate: addDays(new Date(), 7),
-      key: 'selection',
-    },
-  ]);
+
+interface DateRangeModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+
+  minStartDate?: Date;
+  maxEndDate?: Date;
+  dateRange?: Range | undefined;
+  isLoading?: boolean;
+  // eslint-disable-next-line no-unused-vars
+  handleChangeDate?: (info: Range) => void;
+}
+
+const DateRangeModal = (props: DateRangeModalProps) => {
+  const {
+    isOpen,
+    onClose,
+    minStartDate,
+    maxEndDate,
+    isLoading,
+    dateRange,
+    handleChangeDate,
+  } = props;
+
+  const getInitialDateRange = (): Range[] => {
+    if (dateRange) {
+      return [dateRange];
+    }
+    return [
+      {
+        startDate: new Date(),
+        endDate: addDays(new Date(), 7),
+        key: 'selection',
+      },
+    ];
+  };
+
+  const [selectedDateRange, setSelectedDateRange] = useState<Range[]>(
+    getInitialDateRange()
+  );
 
   return (
     <>
-      <DateRangePicker
-        onChange={(item: any) => {
-          if (item.selection) {
-            setState([item.selection]);
-          }
-        }}
-        showPreview
-        moveRangeOnFirstSelection={false}
-        months={2}
-        ranges={state}
-        direction="horizontal"
-        weekStartsOn={1}
-        monthDisplayFormat="LLLL yyy"
-      />
-      ;{' '}
+      <GenericModal
+        isOpen={isOpen}
+        onClose={onClose}
+        contentStyle={{ width: { lg: '560px' } }}
+      >
+        <ModalBody m={0} p={0} py="32px" pl="32px" pr="27px" width="full">
+          <VStack width="full" alignItems="flex-start" spacing="24px">
+            <Text size="lg" color="primary.500" fontWeight={500}>
+              Custom Date
+            </Text>
+
+            <DateRangePicker
+              onChange={(item: any) => {
+                if (!item.selection) return;
+
+                setSelectedDateRange([item.selection]);
+
+                if (handleChangeDate) {
+                  handleChangeDate(item.selection);
+                }
+              }}
+              showPreview
+              moveRangeOnFirstSelection={false}
+              months={2}
+              ranges={selectedDateRange}
+              direction="horizontal"
+              weekStartsOn={1}
+              monthDisplayFormat="LLLL yyy"
+              minDate={minStartDate}
+              maxDate={maxEndDate}
+            />
+          </VStack>
+        </ModalBody>
+
+        <ModalFooter
+          p={0}
+          m={0}
+          width="full"
+          justifyContent="space-between"
+          pl="32px"
+          pr="27px"
+          mb="19px"
+          mt="10px"
+          alignItems="flex-start"
+        >
+          <div>Helo</div>
+
+          <HStack spacing="16px" width="full" justifyContent="flex-end">
+            <Button
+              variant="secondary"
+              customStyles={{ width: '116px' }}
+              handleClick={onClose}
+            >
+              Cancel
+            </Button>
+            <Button
+              customStyles={{ w: '116px' }}
+              handleClick={() => console.log()}
+              isLoading={isLoading}
+              loadingText="Validating..."
+            >
+              Done
+            </Button>
+          </HStack>
+        </ModalFooter>
+      </GenericModal>
     </>
   );
 };
 
-export default DateRange;
+export default DateRangeModal;

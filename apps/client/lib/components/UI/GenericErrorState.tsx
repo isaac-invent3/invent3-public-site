a1967f8119f2570eea46a54bd9ae51f111c91a81
isaac-invent3 <isaac@invent3.ai>
2025-01-06 12:08:13 +0100

    feat: add generic error component

---
+++ apps/client/lib/components/UI/GenericErrorState.tsx
@@ -0,0 +1,25 @@
+import { Heading, Icon, Text, VStack } from '@chakra-ui/react';
+import React from 'react';
+import { InfoIcon } from '../CustomIcons';
+
+interface GenericErrorStateProps {
+  title?: string;
+  subtitle?: string;
+}
+const GenericErrorState = (props: GenericErrorStateProps) => {
+  const { title, subtitle } = props;
+
+  return (
+    <VStack width="full" spacing="16px" height="full" justifyContent="center">
+      <Icon as={InfoIcon} color="error.500" boxSize="40px" />
+      <VStack width="full" spacing="8px">
+        <Heading fontSize="32px" lineHeight="38.02px">
+          {title ?? 'Error'}
+        </Heading>
+        <Text color="neutral.700">{subtitle ?? 'Not Found'}</Text>
+      </VStack>
+    </VStack>
+  );
+};
+
+export default GenericErrorState;

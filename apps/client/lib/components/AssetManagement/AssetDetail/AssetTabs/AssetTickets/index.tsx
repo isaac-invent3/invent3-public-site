4fca7d1b189884143f54f597f0311ad9a9e4bdcd
Drex <david@invent3.ai>
2025-01-17 06:11:51 +0100

    Chore: Refactored the Ticket Table Component

--- apps/client/lib/components/AssetManagement/AssetDetail/AssetTabs/AssetTickets/index.tsx
+++ apps/client/lib/components/AssetManagement/AssetDetail/AssetTabs/AssetTickets/index.tsx
@@ -1,15 +1,8 @@
 import { Flex } from '@chakra-ui/react';
-import { DataTable } from '@repo/ui/components';
-import { createColumnHelper } from '@tanstack/react-table';
-import { useMemo, useState } from 'react';
-import UserInfo from '~/lib/components/Common/UserInfo';
-import GenericStatusBox from '~/lib/components/UI/GenericStatusBox';
-import { Ticket } from '~/lib/interfaces/ticket.interfaces';
+import { useState } from 'react';
+import TicketTable from '~/lib/components/TicketManagement/TicketTable';
 import { useAppSelector } from '~/lib/redux/hooks';
 import { useGetAssetOpenTicketsQuery } from '~/lib/redux/services/ticket.services';
-import { COLOR_CODES_FALLBACK } from '~/lib/utils/constants';
-import { dateFormatter } from '~/lib/utils/Formatters';
-import PopoverAction from './PopoverAction';
 
 const AssetTickets = () => {
   const assetData = useAppSelector((state) => state.asset.asset);
@@ -28,72 +21,6 @@ const AssetTickets = () => {
     assetId,
   });
 
-  const columnHelper = createColumnHelper<Ticket>();
-  const columns = useMemo(
-    () => {
-      const baseColumns = [
-        columnHelper.accessor('ticketId', {
-          cell: (info) => info.getValue(),
-          header: '#',
-          enableSorting: false,
-        }),
-
-        columnHelper.accessor('ticketTitle', {
-          cell: (info) => info.getValue(),
-          header: 'Title',
-          enableSorting: false,
-        }),
-
-        columnHelper.accessor('ticketTypeName', {
-          cell: (info) => info.getValue(),
-          header: 'Type',
-          enableSorting: false,
-        }),
-
-        columnHelper.accessor('ticketPriorityName', {
-          cell: (info) => {
-            const ticket = info.row.original;
-
-            return (
-              <GenericStatusBox
-                colorCode={
-                  ticket.priorityColorCode ?? COLOR_CODES_FALLBACK.default
-                }
-                width="80px"
-                text={ticket.ticketPriorityName}
-              />
-            );
-          },
-          header: 'Priority',
-        }),
-
-        columnHelper.accessor('issueReportDate', {
-          cell: (info) =>
-            dateFormatter(info.getValue(), 'DD / MM / YYYY hh:mma'),
-          header: 'Requested Date',
-          enableSorting: false,
-        }),
-
-        columnHelper.accessor('reportedBy', {
-          cell: (info) => <UserInfo name={info.getValue()} />,
-          header: 'Requested By',
-          enableSorting: true,
-        }),
-
-        columnHelper.accessor('facilityRef', {
-          cell: (info) => (
-            <PopoverAction ticket={info.row.original} category="in_progress" />
-          ),
-          header: '',
-          enableSorting: false,
-        }),
-      ];
-
-      return baseColumns;
-    },
-    [[data?.data?.items]] //eslint-disable-line
-  );
-
   return (
     <Flex
       width="full"
@@ -102,35 +29,14 @@ const AssetTickets = () => {
       direction="column"
       my="32px"
     >
-      <DataTable
-        columns={columns}
-        data={data?.data?.items ?? []}
+      <TicketTable
+        data={data}
         isLoading={isLoading}
         isFetching={isFetching}
-        totalPages={data?.data?.totalPages}
-        setPageNumber={setCurrentPage}
-        pageNumber={currentPage}
+        currentPage={currentPage}
         pageSize={pageSize}
+        setCurrentPage={setCurrentPage}
         setPageSize={setPageSize}
-        emptyLines={5}
-        isSelectable
-        maxTdWidth="200px"
-        customThStyle={{
-          paddingLeft: '16px',
-          paddingTop: '12px',
-          paddingBottom: '12px',
-          fontWeight: 700,
-        }}
-        customTdStyle={{
-          paddingLeft: '16px',
-          paddingTop: '12px',
-          paddingBottom: '12px',
-        }}
-        showFooter={Boolean(
-          data?.data?.totalPages && data?.data?.totalPages > 1
-        )}
-        rowColorKey="priorityColorCode"
-        customTableContainerStyle={{ rounded: 'none' }}
       />
     </Flex>
   );

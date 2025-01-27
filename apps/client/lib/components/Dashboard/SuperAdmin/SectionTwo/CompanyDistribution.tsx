51384041273fc71c25291665ebd4f6f1bfeb6dc7
isaac-invent3 <isaac@invent3.ai>
2025-01-27 18:42:56 +0100

    feat: implement super admin UI

---
+++ apps/client/lib/components/Dashboard/SuperAdmin/SectionTwo/CompanyDistribution.tsx
@@ -0,0 +1,38 @@
+import { HStack, VStack } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { generateLastFiveYears } from '~/lib/utils/helperFunctions';
+import { Option } from '@repo/interfaces';
+import DropDown from '../../Common/DropDown';
+import CardHeader from '../../Common/CardHeader';
+
+const CompanyDistribution = () => {
+  const [selectedYear, setSelectedYear] = useState<Option | undefined>(
+    generateLastFiveYears()[0] as Option
+  );
+
+  return (
+    <VStack
+      height="full"
+      p="20px"
+      alignItems="flex-start"
+      spacing="16px"
+      bgColor="white"
+      rounded="8px"
+    >
+      <HStack width="full" justifyContent="space-between">
+        <CardHeader>Company Distribution</CardHeader>
+        <DropDown
+          options={generateLastFiveYears()}
+          label="Year"
+          handleClick={(option) => {
+            setSelectedYear && setSelectedYear(option);
+          }}
+          selectedOptions={selectedYear ?? null}
+          width="100px"
+        />
+      </HStack>
+    </VStack>
+  );
+};
+
+export default CompanyDistribution;

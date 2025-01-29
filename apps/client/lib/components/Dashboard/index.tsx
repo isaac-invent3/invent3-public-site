f4b15d9cfe1e01f7e8fe88f74c4f461faa11b95f
isaac-invent3 <isaac@invent3.ai>
2025-01-29 13:13:55 +0100

    chore: fix build error

--- apps/client/lib/components/Dashboard/index.tsx
+++ apps/client/lib/components/Dashboard/index.tsx
@@ -1,20 +1,14 @@
 'use client';
 
 import { Flex } from '@chakra-ui/react';
-import React, { useEffect } from 'react';
+import React from 'react';
 // import OperationManager from './OperationManager';
 // import FrontDesk from './FrontDesk';
 // import FieldEngineer from './FieldEngineer';
 // import ClientAdmin from './ClientAdmin';
 import SuperAdmin from './SuperAdmin';
-import usePermissionAccess from '~/lib/hooks/useRoleAccess';
 
 const Dashboard = () => {
-  const hasDashboardAccess = usePermissionAccess('asset');
-  useEffect(() => {
-    console.log({ hasDashboardAccess });
-  }, [hasDashboardAccess]);
-
   return (
     <Flex width="full" direction="column">
       {/* <OperationManager /> */}

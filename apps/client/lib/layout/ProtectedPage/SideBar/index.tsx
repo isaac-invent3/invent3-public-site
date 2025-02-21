25e98d52a832f2fd42782d59c731a4f0b683aad0
isaac-invent3 <isaac@invent3.ai>
2025-02-21 15:52:34 +0100

    chore: Implement sidebar overflow feature

--- apps/client/lib/layout/ProtectedPage/SideBar/index.tsx
+++ apps/client/lib/layout/ProtectedPage/SideBar/index.tsx
@@ -1,11 +1,19 @@
-import { Flex, useOutsideClick, VStack } from '@chakra-ui/react';
+import {
+  Flex,
+  HStack,
+  Icon,
+  Text,
+  useOutsideClick,
+  VStack,
+} from '@chakra-ui/react';
 import { isEmpty, some } from 'lodash';
-import React, { useLayoutEffect, useRef, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { filterSidebarData } from './utils';
 import NavItem from './NavItem';
 import LogoSection from './LogoSection';
 import FooterSection from './FooterSection';
 import { SideBarData } from '~/lib/interfaces/general.interfaces';
+import { ChevronDownIcon } from '~/lib/components/CustomIcons';
 
 interface SideBarProps {
   isCollapse: boolean;
@@ -15,6 +23,29 @@ const SideBar = (props: SideBarProps) => {
   const { isCollapse, setIsCollapse } = props;
   const [sideBarData, setSideBarData] = useState<SideBarData[]>([]);
   const flexRef = useRef(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [hasOverflow, setHasOverflow] = useState(false);
+
+  // Checks for overflow of the sidebar content
+  useEffect(() => {
+    const checkOverflow = () => {
+      if (containerRef.current) {
+        setHasOverflow(
+          containerRef.current.scrollHeight > containerRef.current.clientHeight
+        );
+      }
+    };
+
+    checkOverflow();
+
+    // Observe resizing in case content changes dynamically
+    const resizeObserver = new ResizeObserver(checkOverflow);
+    if (containerRef.current) {
+      resizeObserver.observe(containerRef.current);
+    }
+
+    return () => resizeObserver.disconnect();
+  }, [sideBarData, isCollapse]);
 
   useLayoutEffect(() => {
     const getSideBarData = async () => {
@@ -42,12 +73,11 @@ const SideBar = (props: SideBarProps) => {
       mt="8px"
       ml="8px"
       pt="42px"
-      pb="60px"
+      pb={{ base: '40px', lg: '72px' }}
       height="calc(100vh - 16px)"
       justifyContent="space-between"
       borderTop="1px solid #F3F3F3"
       position={{ base: 'absolute', md: 'fixed' }}
-      overflowY="auto"
       rounded="16px"
       transition="width 0.4s ease"
       bgImage="/layout-bg.png"
@@ -61,7 +91,25 @@ const SideBar = (props: SideBarProps) => {
         <LogoSection isCollapse={isCollapse} />
         {/* Logo Section Ends */}
         {/* Navigation Menu */}
-        <VStack width="full" spacing="8px" px={isCollapse ? '0' : '0px'}>
+        <VStack
+          width="full"
+          spacing="8px"
+          pb="8px"
+          maxH="60vh"
+          overflowY="auto"
+          sx={{
+            scrollbarWidth: '0px',
+            scrollbarColor: 'transparent transparent',
+            '&::-webkit-scrollbar': {
+              width: '0px',
+              height: '0px',
+            },
+            '&::-webkit-scrollbar-thumb': {
+              background: 'transparent',
+            },
+          }}
+          ref={containerRef}
+        >
           {sideBarData.map((item) => (
             <NavItem
               key={item.name}
@@ -72,6 +120,12 @@ const SideBar = (props: SideBarProps) => {
             />
           ))}
         </VStack>
+        {hasOverflow && (
+          <HStack spacing="8px" width="full" justifyContent="center" pt="8px">
+            {!isCollapse && <Text color="white">Scroll Down</Text>}
+            <Icon as={ChevronDownIcon} boxSize="16px" color="white" />
+          </HStack>
+        )}
         {/* Navigation Menu */}
       </Flex>
       {/* Footer starts Here */}

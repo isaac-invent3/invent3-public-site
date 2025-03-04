eb447dfab45a6e6c9e1f5d6a1421c43379484683
isaac-invent3 <isaac@invent3.ai>
2025-03-04 19:20:10 +0100

    chore: 1) Add Signin button 2) Add public route to middleware

--- apps/client/lib/layout/PublicPage/Header/MobileNav.tsx
+++ apps/client/lib/layout/PublicPage/Header/MobileNav.tsx
@@ -9,17 +9,21 @@ import {
   DrawerCloseButton,
   DrawerBody,
   Flex,
+  HStack,
+  Button,
 } from '@chakra-ui/react';
-import { usePathname, useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
-import { HamburgerIcon } from '~/lib/components/CustomIcons/PublicFacingSite';
+import {
+  GlobeIcon,
+  HamburgerIcon,
+} from '~/lib/components/CustomIcons/PublicFacingSite';
 
 import Links from './data';
 import Image from 'next/image';
 
 const MobileNav = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const pathname = usePathname();
   const router = useRouter();
 
   const handleNavigation = (link: string) => {
@@ -50,7 +54,7 @@ const MobileNav = () => {
             <Flex
               position="relative"
               height="35px"
-              width="71px"
+              width="98px"
               flexShrink={0}
               mt="6px"
             >
@@ -59,14 +63,13 @@ const MobileNav = () => {
             <DrawerCloseButton p={0} m={0} />
           </Flex>
           <DrawerBody padding="32px">
-            <VStack width="full" spacing="24px" mt="40px">
+            <VStack width="full" spacing="32px" mt="40px">
               {Links.map((item) => (
                 <Text
-                  color={pathname === item.href ? 'customRed.500' : 'black'}
+                  color="primary.500"
                   size="md"
-                  _hover={{
-                    color: 'customRed.500',
-                  }}
+                  lineHeight="20px"
+                  letterSpacing="0.04em"
                   whiteSpace="nowrap"
                   cursor="pointer"
                   onClick={() => handleNavigation(item.href)}
@@ -74,6 +77,45 @@ const MobileNav = () => {
                   {item.label}
                 </Text>
               ))}
+              <HStack spacing="8px">
+                <Icon as={GlobeIcon} boxSize="24px" color="primary.500" />
+                <Text size="lg" color="primary.500">
+                  EN
+                </Text>
+              </HStack>
+              <VStack spacing="16px">
+                <Button
+                  py="16px"
+                  px="32px"
+                  minH="49px"
+                  rounded="8px"
+                  fontWeight={700}
+                  fontSize="14px"
+                  lineHeight="16.63px"
+                  color="primary.500"
+                  variant="outline"
+                  width="177px"
+                  borderColor="primary.500"
+                  as="a"
+                  onClick={() => handleNavigation('/signin')}
+                >
+                  Sign In
+                </Button>
+                <Button
+                  py="16px"
+                  px="32px"
+                  minH="49px"
+                  rounded="8px"
+                  bgColor="primary.500"
+                  fontWeight={700}
+                  fontSize="14px"
+                  lineHeight="16.63px"
+                  color="white"
+                  width="177px"
+                >
+                  Request a Demo
+                </Button>
+              </VStack>
             </VStack>
           </DrawerBody>
         </DrawerContent>

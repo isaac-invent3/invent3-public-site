81d5ba0ca5e45b9be935a4fe321d781871914586
isaac-invent3 <isaac@invent3.ai>
2025-03-24 11:58:53 +0100

    chore: update middleware

--- apps/client/middleware.ts
+++ apps/client/middleware.ts
@@ -216,6 +216,9 @@ export async function middleware(request: NextRequest) {
     return response;
   }
 
+  if (publicRoutes.includes(pathname)) {
+    return response;
+  }
   return NextResponse.redirect(
     new URL(`/signin?ref=${request.nextUrl.pathname}`, request.url)
   );

652fda28fcca77f95fc0d1ab2dbc6a78feb1235b
isaac-invent3 <isaac@invent3.ai>
2025-07-31 19:07:52 +0100

    chore: hard code GTM ID

--- apps/client/lib/utils/gtm.ts
+++ apps/client/lib/utils/gtm.ts
@@ -1,5 +1,5 @@
 // lib/gtm.ts
-export const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || '';
+export const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || 'GTM-PQ9RB86S';
 
 export const pageview = (url: string) => {
   if (window && (window as any).dataLayer) {
